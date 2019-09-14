a5ab69db9dac940dd968a3572d012d937fbe8571
Harnet69 <harnet69@gmail.com>
2019-09-14 20:31:35 +0200

    Modify an output according to issue requirements, del function now works on two ways, depends of quantity of arguments

--- ideaBank.py
+++ ideaBank.py
@@ -1,10 +1,13 @@
 import sys
 from os import path
+
+
 file_name = "my_ideas_bank"
 
+
+# check an existance and create file if not exists, read ideas from file
 def ideas_reader():
-# check an existance of ideas file and create if not exists, read ideas from text file (file per line)
-    if not path.exists("my_ideas_bank"):
+    if not path.exists(file_name):
         my_ideas_bank = open(file_name, "a+")
     else:
         my_ideas_bank = open(file_name, "r+")
@@ -13,18 +16,18 @@ def ideas_reader():
         return list_of_ideas
 
 
-def display(list_of_ideas):
 # show the list of ideas
-    print("There is the list of your ideas:\n")
+def display(list_of_ideas):
+    print("Your ideabank:\n")
     for index, value in enumerate(list_of_ideas, 1):
-        print(index, value)
+        print(f"{index}. {value}")
 
-def ideas_writer(list_of_ideas):
+
 # save ideas in text file
-    print(list_of_ideas)
+def ideas_writer(list_of_ideas):
     blank_idea = False
     while blank_idea is False:
-        new_users_idea = input("Type your brilliant idea!\n")
+        new_users_idea = input("What is your new idea:\n")
         if new_users_idea == "" or new_users_idea == " ":
             print("Your idea can't be empty")
             blank_idea = False
@@ -37,52 +40,59 @@ def ideas_writer(list_of_ideas):
     print("New idea added")
 
 
-def ideas_deleter(list_of_ideas):
-# delete the idea
-    print("There are your ideas:\n")
-    for index, value in enumerate(list_of_ideas, 1):
-        print(index, value)
-    out_of_range = True
-    while out_of_range:
-        try:
-            delete_id = int(input("Give the number of idea you want to delete: \n"))
-            if delete_id > len(list_of_ideas):
-                print("Your number is out of range!")
+# delete the idea(works on two ways, depends of quantity of arguments)
+def ideas_deleter(list_of_ideas, *argv):
+    print("Your ideabank:\n")
+    if not argv:
+        for index, value in enumerate(list_of_ideas, 1):
+            print(index, value)
+        out_of_range = True
+        while out_of_range:
+            try:
+                delete_id = int(input("Give the number of idea you want to delete:\n"))
+                if delete_id <= 0 or delete_id > len(list_of_ideas):
+                    print("Your don't have idea with such number")
+                    continue
+                out_of_range = False
+            except ValueError:
+                print("Give a correct number!")
                 continue
-            out_of_range = False
-        except ValueError:
-            print("Give a correct number!")
-            continue
-    list_of_ideas.pop(delete_id-1)
-    my_ideas_bank = open(file_name, "w")
-    my_ideas_bank.writelines(list_of_ideas)
-    my_ideas_bank.close()
-    print("The idea was deleted\n")
-    for index, value in enumerate(list_of_ideas, 1):
-        print(index, value)
+    else:
+        delete_id = int(argv[0])
+    try:
+        list_of_ideas.pop(delete_id-1)
+        my_ideas_bank = open(file_name, "w")
+        my_ideas_bank.writelines(list_of_ideas)
+        my_ideas_bank.close()
+        print("The idea was deleted\n")
+        for index, value in enumerate(list_of_ideas, 1):
+            print(index, value)
+    except IndexError:
+        print("Your don't have idea with such number!")
 
 
 def main():
-
-# check_ideas_file()
     if len(sys.argv) == 1:
-    #   Display first idea
+        # Display first idea
         ideas_writer(ideas_reader)
         display(ideas_reader())
-        #first_idea = ideas_reader()
-        #print(first_idea[0])
-    else:
+    elif len(sys.argv) == 2:
         if sys.argv[1] == "--list":
-        # Show all ideas
+            # Show all ideas
             display(ideas_reader())
         elif sys.argv[1] == "--add":
-        # Add a new idea
+            # Add a new idea
             ideas_writer(ideas_reader)
         elif sys.argv[1] == "--del":
-        # Remove an idea by id
+            # Remove an idea by id
             ideas_deleter(ideas_reader())
         else:
             print("Unknown command")
+    elif len(sys.argv) == 3:
+        if sys.argv[1] == "--del":
+            ideas_deleter(ideas_reader(), sys.argv[2])
+    else:
+        print("Unknown command")
 
 
 if __name__ == "__main__":
