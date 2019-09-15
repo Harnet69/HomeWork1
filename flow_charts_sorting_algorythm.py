09bd1f0f6a143a3d41a689382443e2add5511797
Harnet69 <harnet69@gmail.com>
2019-09-15 19:22:42 +0200

    Added file Algorithm from flowchart

---
+++ flow_charts_sorting_algorythm.py
@@ -0,0 +1,23 @@
+def main():
+    numbers = [1, 2 ,56, 32, 51, 2, 8, 92, 15]
+    print(numbers)
+    iteration = 1
+    n = len(numbers)
+
+    while iteration < n:
+        j = 0
+        while j <= n - 2:
+            if numbers[j] > numbers[j+1]:
+                temp = numbers[j+1]
+                numbers[j+1] = numbers[j]
+                numbers[j] = temp
+                j = j + 1
+            else:
+                j = j + 1
+
+        iteration = iteration + 1
+    print(numbers)
+
+
+if __name__ == "__main__":
+    main()
